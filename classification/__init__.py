48274255ba93a800be116594395b7c2ff47b1888
Mehdi Lotifan <79265020+MehdiLotfian@users.noreply.github.com>
2024-05-29 17:53:54 +0200

    Delete classification directory

--- classification/__init__.py
+++
@@ -1,9 +0,0 @@
-from Logic.core.classification.basic_classifier import *
-from Logic.core.classification.data_loader import *
-from Logic.core.classification.deep import *
-from Logic.core.classification.knn import *
-from Logic.core.classification.naive_bayes import *
-from Logic.core.classification.svm import *
-
-
-__all__ = [k for k in globals().keys() if not k.startswith("_")]
